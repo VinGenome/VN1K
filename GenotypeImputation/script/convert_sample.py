69ed70c4f8761ef8964386e4708d458bbce35e87
thamhoang2016 <thamhong.hoang@gmail.com>
2026-03-05 21:15:05 -0500

    Update convert_sample.py

--- GenotypeImputation/script/convert_sample.py
+++ GenotypeImputation/script/convert_sample.py
@@ -1,6 +1,6 @@
 import os
 
-with open('/home/vinhdc/Bioinfomatics/project/1KVG_imputation_panel/Sample/list-apmra96.txt', 'r')  as fp:
+with open('list-apmra96.txt', 'r')  as fp:
     apmra_sample = fp.read().rstrip().split()
     apmraSample = [i.split(".")[0].zfill(4) for i in apmra_sample]
 print(apmraSample)
@@ -14,7 +14,7 @@ for i in apmraSample:
     sample.extend(sampleTmp)
 print(sample, len(sample))
 
-with open('/home/vinhdc/Bioinfomatics/project/1KVG_imputation_panel/Sample/list-VN95.txt', 'w') as fp:
+with open('list-VN95.txt', 'w') as fp:
     fp.write("\n".join(sample))
 # for i in apmraSample:
 #     sampleTmp = [s for s in sample if i in s]
